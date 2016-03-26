5d0dbee7ceec6bb922592966a7c4b04c86acf5f1
OUC-MYM <570109323@qq.com>
2016-03-26 13:04:28 +0800

    add

--- snake/main.cpp
+++ snake/main.cpp
@@ -1,6 +1,6 @@
-#include"uselei.h"
-#include<stdio.h>
-#include"main.h"
+#include "uselei.h"
+#include <stdio.h>
+#include "main.h"
 #include <iostream>
 #include <queue>
 using namespace std;
@@ -39,19 +39,53 @@ int initin(int ks[100000])
     return 0;
 }
 
-class points
+int mapp[50][30][30];
+bool snack2_exist=1;     //1为存在
+
+bool find_snack2()
 {
-    point xy;
-    int dir;
-};
+    for(int i=0; i<ss.p2.bodylen; i++)
+        if(mapp[0][ss.p2.body[i].x][ss.p2.body[i].y]!=1)
+            return false;
+    return true;
+}
 
 int player_1(int ks[100000])
 {
     initin(ks);   //就是在这里调用一下初始函数。
-
     //在这里写大家的程序，直接运用ss即可。
     //比如想获知食物坐标的时候，为：ss.food.x ,ss.food.y
-    return 0;
+    cout<<ss.mpsize << endl;
+    //地图
+    for(int i=0; i<30; i++)
+        for(int j=0; j<30; j++)
+            mapp[0][i][j]=ss.smap[i][j];
+
+    snack2_exist = find_snack2();
+    if(snack2_exist)
+        for(int i=0; i<ss.p2.bodylen; i++)
+            mapp[0][ss.p2.body[i].x][ss.p2.body[i].y]=3;
+
+
+    for(int i=0; i<30; i++)
+    {
+        for(int j=0; j<30; j++)
+            cout << mapp[0][i][j];
+        cout << endl;
+    }
+    int dir;
+
+    cin >> dir;
+    cout << dir << endl;
+    if(dir==6)
+        return 1;
+    if(dir==2)
+        return 2;
+    if(dir==4)
+        return 3;
+    if(dir==8)
+        return 0;
+
 }
 
 extern "C" DLL_EXPORT BOOL APIENTRY DllMain(HINSTANCE hinstDLL, DWORD fdwReason, LPVOID lpvReserved)
